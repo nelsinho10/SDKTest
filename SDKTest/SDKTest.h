6bc3b4b20844df5075514cda6804d7bfdebd0820
Nelson Sambula <nelson.sambula@movitext.com>
2024-06-07 14:48:30 -0600

    Initial Commit

---
+++ SDKTest/SDKTest.h
@@ -0,0 +1,18 @@
+//
+//  SDKTest.h
+//  SDKTest
+//
+//  Created by Nelson Sambula on 7/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKTest.
+FOUNDATION_EXPORT double SDKTestVersionNumber;
+
+//! Project version string for SDKTest.
+FOUNDATION_EXPORT const unsigned char SDKTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKTest/PublicHeader.h>
+
+
